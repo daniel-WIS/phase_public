8a55388da55324416a3b562b5bb5c924dcd9ee60
Daniel <65821587+daniel-WIS@users.noreply.github.com>
2021-06-25 14:49:55 +0300

    Update analysis_functions.R

--- analysis_functions.R
+++ analysis_functions.R
@@ -1,3 +1,6 @@
+##source the pdef file from the directory
+#get all .csv files that have 'pdef' in their name
+
 f.get.pdef <- function(directory){
   pdef_files <- list.files(path = directory,pattern = ".*pdef.*\\.csv")
   if (length(pdef_files)>1) stop("More than 1 pdef file detected. Please keep only 1 pdef file in the directory or specify it manually")
