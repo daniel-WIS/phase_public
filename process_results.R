160f69f22d08090723d09bc30e201af2ab33c597
d <d.osterle@student.vu.nl>
2021-10-27 19:02:35 +0200

    all

--- process_results.R
+++ process_results.R
@@ -1,32 +1,5 @@
 #!/usr/bin/env Rscript
-
-print("This script will automatically look for all required input files in the current dir:")
-print(getwd())
-print("Use options to specify custom input files/paths.")
-print("To see all available options run: ./process_results.R --help")
-print("It is sufficient to specify individual custom options. The other options will be determined automatically from the current dir.")
-#ANSWER <- readline("Continue? [y/n]")
-#if (ANSWER != "y" | ANSWER != "Y") {stop()}
-
 library("optparse")
-
-
-#Check which OS ("Darwin" is MacOS)
-if (Sys.info()['sysname'] == "Darwin") {
-  os.foldername <- "/Volumes"
-  source("/Users/d/phase/Multiwell.R")
-  source("/Users/d/phase/general.R")
-  source("/Users/d/phase/MicroscopeToolBox.R")
-  
-}else if (Sys.info()['sysname'] == "Linux") {
-  os.foldername <- "/media"
-  source("/home/d/phase_private_final/uscope_tools/Multiwell.R")
-  source("/home/d/phase_private_final/uscope_tools/general.R")
-  source("/home/d/phase_private_final/uscope_tools/MicroscopeToolBox.R")
-}
-
-#source("/home/d/phase_public_final/postprocess_result_txt.R")
-#source("/home/d/phase_public_final/valvar_diploid_plot_functions.R")
 
 option_list = list(
   make_option(c("-d","--working_dir"), type="character", default=NULL, 
@@ -49,13 +22,67 @@ option_list = list(
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 
-if(is.null(opt$working_dir)){opt$working_dir <- getwd()}
+print("This script will automatically look for all required input files in the current dir:")
+print(getwd())
+print("Use options to specify custom input files/paths.")
+print("To see all available options run: ./process_results.R --help")
+print("It is sufficient to specify individual custom options. The other options will be determined automatically from the current dir.")
+
+cat("Continue? [y/n] ")
+ANSWER <- readLines("stdin",n=1)
+if (ANSWER != "y" & ANSWER != "Y") {stop("User interrupt")}
+
+
+#Check which OS ("Darwin" is MacOS)
+if (Sys.info()['sysname'] == "Darwin") {
+  os.foldername <- "/Volumes"
+  source("/Users/d/phase/Multiwell.R")
+  source("/Users/d/phase/general.R")
+  source("/Users/d/phase/MicroscopeToolBox.R")
+  
+}else if (Sys.info()['sysname'] == "Linux") {
+  os.foldername <- "/media"
+  source("/home/d/phase_private_final/uscope_tools/Multiwell.R")
+  source("/home/d/phase_private_final/uscope_tools/general.R")
+  source("/home/d/phase_private_final/uscope_tools/MicroscopeToolBox.R")
+}
+
+#source("/home/d/phase_public_final/postprocess_result_txt.R")
+#source("/home/d/phase_public_final/valvar_diploid_plot_functions.R")
+
+
+if(is.null(opt$working_dir)) {
+  opt$working_dir <- getwd()
+}
 
 if(is.null(opt$nd_folder_path)){
   #search for .nd file. save to opt$nd_folder_path
+  opt$nd_folder_path <- list.files(path = opt$working_dir, pattern = ".nd$", full.names = TRUE, recursive = TRUE)
+  if (length(opt$nd_folder_path)>1) stop("More than 1 .nd file detected. Please keep only 1 .nd file in the directory or specify it manually")
+  if (length(opt$nd_folder_path)<1) stop("No .nd file detected. Please keep exactly 1 file in the directory. The name has to contain the word .nd")
   }
 
+if (is.null(opt$pdef_path)) {
+  #get all .csv files that have 'pdef' in their name
+  opt$pdef_path <- list.files(path = opt$working_dir, pattern = ".*pdef.*\\.csv", full.names = TRUE, recursive = TRUE)
+  if (length(opt$pdef_path)>1) stop("More than 1 pdef file detected. Please keep only 1 pdef file in the directory or specify it manually")
+  if (length(opt$pdef_path)<1) stop("No pdef file detected. Please keep exactly 1 file in the directory. The name has to contain the word 'pdef' and it must be a .csv file.")
+}
 
+if (is.null(opt$results_txt_path)) {
+  #get the folder that contains the result txt files
+  opt$results_txt_path <- list.files(path = opt$working_dir, pattern = "images_output", full.names = TRUE, recursive = TRUE, include.dirs = TRUE, ignore.case = TRUE)
+  if (length(opt$results_txt_path)>1) stop("More than 1 results folder detected. Please keep only 1 folder in the directory or specify it manually")
+  if (length(opt$results_txt_path)<1) stop("No results folder detected. Please keep exactly 1 folder in the directory. The name has to be images_output (not case sensitive)")
+}
+
+cat(
+  "The current working directory is: ", opt$working_dir, "\n",
+  "The .nd file path is: ",opt$nd_folder_path, "\n",
+  "The pdef file path is: ",opt$pdef_path, "\n",
+  "The path to the folder containing the result txt files is: ",opt$results_txt_path, "\n",
+  sep = ""
+)
 
 # pdef_head <- colnames(read.csv(opt$pdef_path,sep=",")) #get column names of pdef file
 # CHANNELS_INPUT <- unlist(lapply(pdef_head,function(x) if(x=="GFP" | x=="RFP" | x=="BFP") x))
